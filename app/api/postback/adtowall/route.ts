e6aefb9ed188f249975e57880766c8cf7f75c0d4
muosab2008-sys <muosab2008@gmail.com>
2026-06-06 22:47:52 +0300

    Update route.ts

--- app/api/postback/adtowall/route.ts
+++ app/api/postback/adtowall/route.ts
@@ -1,4 +1,4 @@
-\import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 import { adminDb } from '@/lib/firebase-admin'; 
 import admin from 'firebase-admin';
 
