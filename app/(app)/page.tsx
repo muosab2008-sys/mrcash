47fd260247ddcc6d33d5d604c270b276ef5eda3d
muosab2008-sys <muosab2008@gmail.com>
2026-06-07 11:08:52 +0200

    Update page.tsx

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -195,7 +195,7 @@ export default function EarnPage() {
     const uid = userData.uid;
     const urls: Record<string, string> = {
       playtime: `https://web.playtimeads.com/index.php?app_id=6d186de0e9e5e8d7&user_id=${uid}`,
-      pubscale: `https://wow.pubscale.com?app_id=99429038&user_id=${uid}`,
+      pubscale: `https://wow.pubscale.com?app_id=18233528user_id=${uid}`,
       gemiad: `https://gemiwall.com/69c1622e82a1cd59c17a2e21/${uid}`,
       revtoo: `https://revtoo.com/offerwall/xol9xws01wsarkpuv7miwdair6ikvu/${uid}`,
       offery: `https://offery.io/offerwall/c9dow9gf6dwikkp48nnlxxynr24ng6/${uid}`,
