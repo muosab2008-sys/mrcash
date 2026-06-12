fbc9ef73bd8fa3bf03c26bf62208ed5ce383457e
muosab2008-sys <muosab2008@gmail.com>
2026-06-12 14:53:02 +0200

    Update page.tsx

--- app/(app)/page.tsx
+++ app/(app)/page.tsx
@@ -44,6 +44,7 @@ const defaultOfferwalls: Offerwall[] = [
   { id: "taskwall", name: "TaskWall", description: "Complete premium tasks and high-paying offers with TaskWall", logoUrl: "http://publishers.taskwall.io//manager/uploads/logo-2.png", avgPoints: 2100, isActive: true, url: "#", color: "#10b981", likes: 41, dislikes: 11, isHot: true },
   { id: "flexwall", name: "Flex Wall", description: "Complete high-paying offers and premium tasks with Flex Wall", logoUrl: "https://mistcash.co/assets/images/networks/69f9fedd46a09.png", avgPoints: 2200, isActive: true, url: "#", color: "#6366f1", likes: 29, dislikes: 6, isHot: false },
   { id: "tplayad", name: "Tplayad", description: "Complete high-paying offers and premium tasks with Flex Wall", logoUrl: "https://mistcash.co/assets/images/networks/68b3359a3c6e5.png", avgPoints: 2200, isActive: true, url: "#", color: "#6366f1", likes: 29, dislikes: 6, isHot: false },
+    { id: "klink", name: "klink", description: "Complete premium tasks and high-paying offers with TaskWall", logoUrl: "https://mistcash.co/assets/images/networks/69d9950fabe44.png", avgPoints: 2100, isActive: true, url: "#", color: "#10b981", likes: 41, dislikes: 11, isHot: true },
 ];
 
 interface VoteData {
@@ -200,6 +201,7 @@ export default function EarnPage() {
       taskwall: `https://wall.taskwall.io/?app_id=e723adebdbab293255deefe5fe401b43&userid=${uid}`,
       flexwall: `https://flexwall.net/iframe?app_id=412&user_id=${uid}`,
       tplayad: `https://tplayad.com/offer/Br9Dd7/${uid}`,
+      klink: `https://offerwall.klinkfinance.com/wall?pub_id=a8d01294-6455-411d-8f03-cc1d716c241d&user_id=${uid}`,
     };
     return urls[wall.id] || wall.url;
   };
